917ac72be08720651f6b5136219fb4f46ffb0aa4
vivek kumar <43455188+yaduvanshi777@users.noreply.github.com>
2021-02-08 13:22:47 +0530

    Update comment_wiper.c

--- comment_wiper.c
+++ comment_wiper.c
@@ -1,41 +1,33 @@
 #include"stdio.h"
 
-int main()
-{
-	FILE* in = fopen("in.c","r");
-	FILE* out = fopen("out.c","w");
-	char c;
-	
-	while ((c = fgetc(in)) != EOF )
-	{
-		if (c=='/')
-		{
-			int flag = 0;
-			c = fgetc(in);
-			if (c=='/')
-			{
-				flag = 1;
-				while ((c=fgetc(in)) != '\n');
-			///	break;	
-			}
-			else if (c=='*')
-			{
-				flag=1;
-				while ((c = fgetc(in)) != EOF)
-				{
-					if ((c = fgetc(in)) == '*' && (c = fgetc(in)) == '/')
-					{
-						c = '\n';
-						break;
-					}				
-				}
-			}
-			if (!flag)
-				fputc('/',out);
-		}
-	fputc(c,out);
-	}
-	fclose(in);
-	fclose(out);
-	return 0;
+int main() {
+    FILE * in = fopen("in.c", "r");
+    FILE * out = fopen("out.c", "w");
+    char c;
+
+    while ((c = fgetc( in )) != EOF) {
+        if (c == '/') {
+            int flag = 0;
+            c = fgetc( in );
+            if (c == '/') {
+                flag = 1;
+                while ((c = fgetc( in )) != '\n');
+                ///	break;	
+            } else if (c == '*') {
+                flag = 1;
+                while ((c = fgetc( in )) != EOF) {
+                    if ((c = fgetc( in )) == '*' && (c = fgetc( in )) == '/') {
+                        c = '\n';
+                        break;
+                    }
+                }
+            }
+            if (!flag)
+                fputc('/', out);
+        }
+        fputc(c, out);
+    }
+    fclose( in );
+    fclose(out);
+    return 0;
 }
